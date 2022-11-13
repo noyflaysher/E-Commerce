ad03e6dbb48ab4e8c53818904091176c68482a2d
noyflaysher <noyflaysher12@gmail.com>
2022-11-13 10:22:11 +0200

    header component

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "Home.css";
+
+function Home() {
+  return (
+    <div>
+        <div className="home">
+            <div className="home__container">
+                <img src="https://www.x-cart.com/wp-content/uploads/2019/01/ecommerce-768x278.jpg" alt="home img" className="home__image" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
